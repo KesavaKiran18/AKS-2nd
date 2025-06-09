6327a9527c24def95365c7b2e4052956155792de
Ubuntu <kesavakiran@devops-VM.2ufpurabv0neve4elbu12cuc5e.bx.internal.cloudapp.net>
2025-06-09 07:27:53 +0000

    Initial commit - AKS Flask app and manifests

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    message = os.getenv('APP_MESSAGE', 'Hello from Flask on AKS! (Default)')
+    return message
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
